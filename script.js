e06aa474d07993a9aad50a5f1a48dea24673bcda
Leonardo Alzogaray <leonardoalzogaray@gmail.com>
2026-03-20 21:44:00 -0300

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * Proyecto: Bitácora Técnica - Leonardo Alzogaray
+ * Función: Control de interactividad y métricas.
+ * Estado: Inicializado.
+ */
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Sistema listo. Arquitectura modular desplegada.");
+});
